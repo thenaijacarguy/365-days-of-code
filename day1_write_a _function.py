9c227477443f915f37375abdf1e297abe332534e
James Gabriel <jamesozgabriel808@gmail.com>
2026-01-01 22:51:16 +0100

    Update and rename write_a _function.py to day1_write_a _function.py

--- day1_write_a _function.py
+++ day1_write_a _function.py
@@ -1,8 +1,6 @@
 # Question: https://www.hackerrank.com/challenges/write-a-function/problem?isFullScreen=true
 # I tried a number of iterations but i couldnt pass all 6 test cases so i checked the internet and it turned out i got some brackets and logic incorrectly.
 
-# My solution
-
 def is_leap(year):
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         return True
